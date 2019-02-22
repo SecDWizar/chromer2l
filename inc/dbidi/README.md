a0b1bca45f40538c894b59fff94d0bae6dda43eb
Nitzan Zaifman <nitzanz@gmail.com>
2019-02-22 17:39:06 +0200

    POC start real r2l engine using ibm-js dbidi
    add require.js to inc/ from https://requirejs.org/docs/release/2.3.6/comments/require.js
    add dbidi to inc/ via git clone git://github.com/ibm-js/dbidi.git (creates inc/dbidi)
    add dcl (dbidi dependency) to inc/dbidi via git clone git://github.com/uhop/dcl.git (creates inc/dbidi/dcl), prob. don't need all(!)
    add decore (dbidi dependency) to inc/dbidi via git clone git://github.com/ibm-js/decor.git (creates inc/dbidi/decor)
    add requirejs-dplugins (some ibm-js dependency) to inc/dbidi via git clone git://github.com/ibm-js/requirejs-dplugins.git (creates inc/dbidi/requirejs-dplugins)
    TODO some cleanups (if we're to use this, prob. change to something else)
    TODO check licensing

---
+++ inc/dbidi/README.md
@@ -0,0 +1,45 @@
+#dbidi [![Build Status](https://travis-ci.org/ibm-js/dbidi.png?branch=master)](https://travis-ci.org/ibm-js/dbidi)
+
+This project provides bidi utilities for JavaScript/AMD/Dojo Applications
+
+## Status
+
+The most recent pre-release is 0.3.0.
+
+## Migration
+
+Replace any use of "dojox/string/BidiEngine" AMD module by "dbidi/TextLayoutEngine".
+
+## Licensing
+
+This project is distributed by the Dojo Foundation and licensed under the ["New" BSD License](https://github.com/dojo/dojo/blob/master/LICENSE#L13-L41).
+All contributions require a [Dojo Foundation CLA](http://dojofoundation.org/about/claForm).
+
+## Dependencies
+
+This project requires the following other projects to run:
+ * dcl
+ * decor
+ 
+## Installation
+
+_Bower_ release installation:
+
+    $ bower install dbidi
+
+_Manual_ master installation: go to the root installation directory and clone dbidi from github:
+
+    $ git clone git://github.com/ibm-js/dbidi.git
+
+Then install dependencies with bower (or manually from github if you prefer to):
+
+	$ cd dbidi
+	$ bower install
+	
+## Documentation
+
+Documentation is available [here](./docs/index)
+
+## Credits
+
+
--- js/background.js
+++ js/background.js
@@ -1,3 +1,28 @@
+/*function getContentFromClipboard() {
+    var clipboardText = '';
+    var t = document.getElementById("sandbox");
+    
+    t.value = '';
+    t.select();
+    if (document.execCommand('paste')) {
+        clipboardText = t.value;
+        console.log ("got clipboardText: " + clipboardText);
+    }
+    t.value = '';
+    
+    return clipboardText
+}
+
+function setContentToClipboard(text) {
+    var t = document.getElementById("sandbox");
+    
+    t.textContent = text;
+    t.select();
+    document.execCommand('copy');
+    t.value = '';
+    //return document.execCommand('copy');
+}*/
+
 function getContentFromClipboard() {
     var clipboardText = '';
     var t = document.createElement("input");
@@ -15,7 +40,7 @@ function getContentFromClipboard() {
 
 function setContentToClipboard(text) {
     var t = document.createElement("textarea");
-    
+
     t.textContent = text;
     document.body.appendChild(t);
     t.select()
@@ -24,8 +49,64 @@ function setContentToClipboard(text) {
     document.body.removeChild(t);
 }
 
-function reverseHebrewText (text) {
-    return text.split("").reverse().join("");
+//function reverseHebrewText (text) {
+function reverseHebrewText (text, callback) {
+    //original from Feb. 21 2019
+    //return text.split("").reverse().join("");
+    
+    //from Feb. 22 2019, initial POC using IBM dbidi
+    require.config({
+        baseUrl: "inc/dbidi"
+    });
+
+    require([
+        "TextLayoutEngine"
+    ], function (TextLayoutEngine) {
+        var bdEngine = new TextLayoutEngine();
+        var newText;
+
+        //bdEngine.inputFornat = "VLYNN";
+        /* 
+        bdEngine.outputFormat = "VLNNN";:
+            orig text: םולש english ייב
+            new text: שלום english ביי
+        bdEngine.outputFormat = "VRNNN";:
+            orig text: שלום english ביי
+            new text: ביי hsilgne שלום
+        bdEngine.outputFormat = "VCNNN";:
+            orig text: שלום english ביי
+            new text: ביי hsilgne שלום
+        bdEngine.outputFormat = "VDNNN";:
+            orig text: שלום english ביי
+            new text: ביי hsilgne שלום
+        */
+        /*      
+        //var newText1 = bdEngine.bidiTransform(text,"ILNNN","VLNNN");
+        bdEngine.outputFormat = "ICNNN";
+        bdEngine.outputFormat = "IDNNN";
+        var newText1 = bdEngine.bidiTransform(text);
+        console.log ("newText1 text: " + newText1);
+
+        //var newText2 = bdEngine.bidiTransform(newText1,"VRNNN","VDNNN");
+        bdEngine.inputFornat = "ICNNN";
+        bdEngine.outputFormat = "VLNNN";
+        var newText2 = bdEngine.bidiTransform(newText1);
+        console.log ("newText2 text: " + newText2);
+
+        //bdEngine.outputFormat = "VLNNN";
+        //var newText3 = bdEngine.bidiTransform(newText2);
+        //console.log ("newText3 text: " + newText3);
+        
+        //newText = newText2;
+        */
+        
+        //console.log ("orig text: " + text);
+        newText = bdEngine.bidiTransform(bdEngine.bidiTransform(text,"ICNNN","IDNNN"),"ICNNN","VLNNN");
+        //console.log ("new text: " + newText);
+
+        //return newText;
+        callback(newText);
+    });
 }
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
@@ -33,16 +114,25 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
         //console.log ('onMessage reverse');
         var clipboardText = getContentFromClipboard();
         
-        reversedText = reverseHebrewText(clipboardText)
-        setContentToClipboard(reversedText);
-        sendResponse();
+        //reversedText = reverseHebrewText(clipboardText)
+        //setContentToClipboard(reversedText);
+        //sendResponse();
+
+        reverseHebrewText(clipboardText, function(newText) {
+            console.log ("in callback reverseHebrewText: " + newText);
+            setContentToClipboard(newText);
+            sendResponse();
+        });
     }
 });
     
 chrome.commands.onCommand.addListener(function(command) {
     if (command === 'r2l') {
         chrome.tabs.executeScript({
-            code: 'document.execCommand("copy"); chrome.runtime.sendMessage({"action": "reverse"}, function() { document.execCommand("paste"); });'
+            // this is just a POC workaround for too quick paste, TODO fix (eventhough everthing is correctly callbacked stacked... so why is the paste before the copy?!)
+            //code: 'document.execCommand("copy"); chrome.runtime.sendMessage({"action": "reverse"}, function() { document.execCommand("paste"); });'
+            //code: 'document.execCommand("copy"); chrome.runtime.sendMessage({"action": "reverse"}, function() { alert ("pause"); document.execCommand("paste"); });'
+            code: 'document.execCommand("copy"); chrome.runtime.sendMessage({"action": "reverse"}, function() { setTimeout (function() { document.execCommand("paste"); }, 100); });'
         });
     }
 });
